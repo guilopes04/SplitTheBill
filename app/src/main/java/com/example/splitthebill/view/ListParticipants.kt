8e62133db47ecab85c5358ce42da256b8542f011
Guilherme <guilhermelopes035@gmail.com>
2023-10-28 19:47:26 -0300

    feat: adiciona classe da tela de participants

---
+++ app/src/main/java/com/example/splitthebill/view/ListParticipants.kt
@@ -0,0 +1,12 @@
+package com.example.splitthebill.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.splitthebill.R
+
+class ListParticipants : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_participants)
+    }
+}
