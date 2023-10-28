bedae3a2607dee4504f43a77556cde1f172cd8ee
Guilherme <guilhermelopes035@gmail.com>
2023-10-28 19:57:13 -0300

    feat: implementa objeto para types

---
+++ app/src/main/java/com/example/splitthebill/model/ConstantTypes.kt
@@ -0,0 +1,7 @@
+package com.example.splitthebill.model
+
+object ConstantTypes {
+    const val EXTRA_PERSON = "EXTRA_PERSON"
+    const val VIEW_PERSON = "VIEW_PERSON"
+    const val BILL_PEOPLE = "BILL_PEOPLE"
+}
